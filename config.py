70e073d5cf32af2ef68b61009f0bef4f4501407f
Turkova Maria <kvm1990@mail.ru>
2019-10-11 15:22:10 +0300

    add MAX_RETRIES=0

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ STATICDIR = os.path.join(BASEDIR, 'static')
 QSS_PATH = os.path.join(STATICDIR, 'qss', 'style.qss')
 CREDENTIALS_PATH = os.path.join(BASEDIR, 'my_credentials.txt')
 LOGO_PATH = os.path.join(STATICDIR, 'logo.png')
+
+MAX_RETRIES=0
