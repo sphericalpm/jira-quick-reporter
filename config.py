ad7e51740ebc5b59653f82dac1fe63e446ff9f32
Turkova Maria <kvm1990@mail.ru>
2019-10-11 22:46:11 +0300

    add comment to MAX_RETRIES constant

--- config.py
+++ config.py
@@ -6,4 +6,5 @@ QSS_PATH = os.path.join(STATICDIR, 'qss', 'style.qss')
 CREDENTIALS_PATH = os.path.join(BASEDIR, 'my_credentials.txt')
 LOGO_PATH = os.path.join(STATICDIR, 'logo.png')
 
-MAX_RETRIES = 0
+MAX_RETRIES = 0  # we need it because without it our app will not be 
+# available (for 15 sec) in case of bad connection or IP blocking
