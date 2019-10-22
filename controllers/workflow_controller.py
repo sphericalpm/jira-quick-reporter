7e213a844d6bc3066a12cab0e43edff5e770a288
Turkova Maria <kvm1990@mail.ru>
2019-10-22 16:45:18 +0300

    add save estimate, comment, find_accound_id_fro_user()

--- controllers/workflow_controller.py
+++ controllers/workflow_controller.py
@@ -1,7 +1,11 @@
 from PyQt5.QtWidgets import QMessageBox
 from jira import JIRAError
+import requests
+from requests.auth import HTTPBasicAuth
+import json
 
 from workflow_window import WorkflowWindow
+from config import CREDENTIALS_PATH, API_LINK_FOR_ACCOUNT_ID
 
 
 class WorkflowController:
@@ -15,6 +19,35 @@ class WorkflowController:
         self.view = WorkflowWindow(self.issue_obj, self.status, self)
         self.view.show()
 
+    def find_account_id_for_user(self, assignee):
+        with open(CREDENTIALS_PATH, 'r', encoding='utf-8') as file:
+            content = file.readline()
+            email, token = content.split(';')
+
+        url = API_LINK_FOR_ACCOUNT_ID.format(assignee)
+        auth = HTTPBasicAuth(email, token)
+
+        headers = {
+            "Accept": "application/json"
+        }
+
+        response = requests.request(
+            "GET",
+            url,
+            headers=headers,
+            auth=auth
+        )
+
+        try:
+            decode_response = json.loads(response.text)
+            account_id = decode_response[0]['accountId']
+
+            return account_id
+
+        except json.decoder.JSONDecodeError as e:
+            QMessageBox.about(self.view, 'Error', e.text)
+
+
     def save_click(self):
         assignee = self.view.assignee_line.text()
         original_estimate = self.view.original_estimate_line.text()
@@ -22,14 +55,31 @@ class WorkflowController:
         comment = self.view.comment_line.toPlainText()
 
         if assignee != "Me":
-            from receive_accountId import find_accountId_for_user
-            account_id = find_accountId_for_user(assignee)
+            account_id = self.find_account_id_for_user(assignee)
 
             if account_id:
                 try:
                     self.jira_client.client.assign_issue(self.issue_obj, account_id)
                 except JIRAError as e:
                     QMessageBox.about(self.view, 'Error', e.text)
+                    return
+            else:
+                QMessageBox.about(self.view, 'Error', 'Account not found')
+                return
 
+        if comment != "":
+            self.jira_client.client.add_comment(self.issue_obj, comment)
 
-        return assignee, original_estimate, remaining_estimate, comment
+        try:
+            self.issue_obj.update(
+                fields={
+                    'timetracking': {
+                        'remainingEstimate': remaining_estimate,
+                        'originalEstimate': original_estimate,
+                    }
+                }
+            )
+        except JIRAError as e:
+            QMessageBox.about(self.view, 'Error', e.text)
+
+        self.view.close()
