c4729d8f46398388d22d3994e258c86c6a36cf94
Turkova Maria <kvm1990@mail.ru>
2019-10-29 11:39:53 +0300

    receive possible_resolutions moved to complete_workflow_cntroller

--- workflow_window.py
+++ workflow_window.py
@@ -67,10 +67,11 @@ class WorkflowWindow(CenterWindow, QMainWindow):
             self.controller.save_click()
 
 class CompleteWorflowWindow(TimeLogWindow):
-    def __init__(self, controller, issue, assignee, save_callback=None):
+    def __init__(self, controller, issue, assignee, possible_resolutions, save_callback=None):
         self.controller = controller
         self.issue = issue
         self.assignee = assignee
+        self.possible_resolutions = possible_resolutions
         super().__init__(issue, save_callback=save_callback)
 
     def build_issue_form_vbox(self):
@@ -86,8 +87,7 @@ class CompleteWorflowWindow(TimeLogWindow):
         self.set_resolution = QComboBox(self)
         vbox.addWidget(self.set_resolution)
 
-        possible_resolutions = self.controller.jira_client.get_possible_resolutions(self.issue)
-        self.set_resolution.addItems(possible_resolutions)
+        self.set_resolution.addItems(self.possible_resolutions)
         self.set_resolution.setCurrentIndex(0)
 
         fix_versions = QLabel('Fix versions:')
