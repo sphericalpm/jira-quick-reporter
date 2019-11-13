4374831eebc65738bf2fe6363eb1aab1cafbbd53
dschglv <dayana.shcheglova@gmail.com>
2019-11-13 20:45:26 +0300

    fixed race condition problem

--- controllers/mixins.py
+++ controllers/mixins.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtCore import QMutex
 
 from controllers.loading_indicator import Thread
 
@@ -37,6 +38,8 @@ class TimeLogMixin:
 
 
 class ProcessWithThreadsMixin:
+    mutex = QMutex()
+
     def __init__(self):
         self.finish_thread_callback = None
         self.error_message = None
@@ -45,7 +48,7 @@ class ProcessWithThreadsMixin:
     def start_loading(self, started_callback, finished_callback):
         self.finish_thread_callback = finished_callback
         self.indicator.spinner.start()
-        self.new_thread = Thread(started_callback, self.error_message)
+        self.new_thread = Thread(started_callback, self.mutex, self.error_message)
         self.new_thread.start()
         self.new_thread.finished.connect(self.stop_loading)
 
@@ -53,3 +56,4 @@ class ProcessWithThreadsMixin:
         self.indicator.spinner.stop()
         self.finish_thread_callback(error_text)
         self.error_message = None
+        self.mutex.unlock()
